edd41fdabfc50eba818bffdd8f3b0f45925c0989
Llewellyn <support@zekini.com>
2021-02-14 00:13:37 +0200

    Initial commit

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Unauthenticated;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithFaker;
+
+    const END_POINT = '/api/users';
+
+    public function testStore()
+    {
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        $dataArray = [
+            'email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name,
+            'password' => '12345678',
+        ];
+
+        $this->post(self::END_POINT, $dataArray, $headers)
+        ->dump()
+            ->assertStatus(201)
+            ->assertJsonStructure(['data']);
+    }
+}
